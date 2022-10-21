73ed96a33fcf84f4c5e858a3a460ddac50e18b08
Amanda Zambelli <amandazambelli@ymail.com>
2022-10-21 15:20:18 -0300

    feat: criando interfaces de Model, Vehicle e Car

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+import { IVehicle } from './IVehicle';
+
+const carZodSchema = z.object({
+  doorsQty: z.number().int().gte(2).lte(4),
+  seatsQty: z.number().int().gte(2).lte(7),
+});
+
+type ICarSchema = z.infer<typeof carZodSchema>;
+
+export interface ICar extends IVehicle, ICarSchema {
+
+}
